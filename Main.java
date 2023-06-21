25941464d66970b4d63b8b6e82814c0783acac12
oksanaverkh <oksana-tyryatkina@yandex.ru>
2023-06-21 16:15:31 +0300

    Final commit

--- Main.java
+++ Main.java
@@ -1,21 +1,21 @@
+// 1) Необходимо реализовать метод разворота связного односвязного списка 
+
 public class Main {
     public static void main(String[] args) {
         LinList list = new LinList();
-        list.removeFirst();
+        LinList reversedList = new LinList();
         list.addFirst(1);
         list.addFirst(2);
         list.addFirst(3);
         list.addFirst(4);
-        list.removeFirst();
-        list.addLast(110);
-        list.addLast(220);
+        list.addFirst(110);
+        list.addFirst(220);
         list.addFirst(0);
-        list.removeLast();
 
         list.print();
+
         System.out.println();
-        if (!list.contains(110)) {
-            System.out.println("No such element");
-        }
+        reversedList = list.reversal(list);
+        reversedList.print();
     }
 }
